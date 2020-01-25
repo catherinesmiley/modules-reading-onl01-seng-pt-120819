2944b659f727058836c2081a81c77dcc99e8bb65
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-25 06:28:48 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,5 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 
 class Kid 
   
@@ -11,4 +12,5 @@ class Kid
   
  extend FancyDance::ClassMethods
  include FancyDance::InstanceMethods
+ 
 end 
